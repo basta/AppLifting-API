1ebbb6060a4844be007e67fe8cb8cf57ae3bb902
basta <jsembasta@gmail.com>
2022-05-19 16:40:33 +0200

    added almbic config folder

---
+++ generic/versions/a98fc03a4d7f_initial_migration.py
@@ -0,0 +1,24 @@
+"""initial migration
+
+Revision ID: a98fc03a4d7f
+Revises: 
+Create Date: 2022-05-17 23:35:42.108952
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a98fc03a4d7f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
